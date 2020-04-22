3867e622df6b218d2df1344ed86a9ef40ebf17b6
yuriy.shpak <yuriy.shpak@itmint.ca>
2020-04-22 03:32:06 +0300

    Add fixes

--- Geometric-figures/Geometric-figures/Program.cs
+++ Geometric-figures/Geometric-figures/Program.cs
@@ -11,7 +11,8 @@ namespace Geometric_figures
         Triangle
     }
 
-    public delegate void Calculator(int paramA = 1, int paramB = 1, int paramС = 1);
+    public delegate Calculator Functional(Calculator delegate1, Calculator delegate2);
+    public delegate string Calculator(int paramA = 1, int paramB = 1, int paramС = 1);
 
     public class Program
     {
@@ -19,27 +20,42 @@ namespace Geometric_figures
         {
             var figures = new Dictionary<Figure, Calculator>();
 
-            Calculator circle = (radius, paramB, paramС) => Console.WriteLine("Circle area = {0}", Math.PI * Math.Pow(radius, 2));
-            circle += (radius, paramB, paramС) => Console.WriteLine("Circle perimeter = {0}", 2 * Math.PI * radius);
+            Calculator circleArea = (radius, paramB, paramС) => $"Circle area = {Math.PI * Math.Pow(radius, 2)}\n";
+            Calculator circlePerimeter = (radius, paramB, paramС) => $"Circle perimeter = {2 * Math.PI * radius}\n";
 
-            Calculator square = (side, paramB, paramС) => Console.WriteLine("Square area = {0}", side * side);
-            square += (side, paramB, paramС) => Console.WriteLine("Square perimeter = {0}", side * 4);
+            Calculator squareArea = (side, paramB, paramС) => $"Square area = {side * side}\n";
+            Calculator squarePerimeter = (side, paramB, paramС) => $"Square perimeter = {side * 4}\n";
 
-            Calculator rectangle = (sideA, sideB, paramС) => Console.WriteLine("Rectangle area = {0}", sideA * sideB);
-            rectangle += (sideA, sideB, paramС) => Console.WriteLine("Rectangle perimeter = {0}", (sideA + sideB) * 2);
+            Calculator rectangleArea = (sideA, sideB, paramС) => $"Rectangle area = {sideA * sideB}\n";
+            Calculator rectanglePerimeter = (sideA, sideB, paramС) => $"Rectangle perimeter = {(sideA + sideB) * 2}\n";
 
-            Calculator triangle = (sideBase, height, sideC) => Console.WriteLine("Triangle area = {0}", sideBase * height / 2);
-            triangle += (sideA, sideB, sideC) => Console.WriteLine("Triangle perimeter = {0}", sideA + sideB + sideC);
+            Calculator triangleArea = (sideBase, height, sideC) => $"Triangle area = {sideBase * height / 2}\n";
+            Calculator trianglePerimeter = (sideA, sideB, sideC) => $"Triangle perimeter = {sideA + sideB + sideC}\n";
+            
+            Functional add = (d1, d2) => (paramA, paramB, paramС) => d1.Invoke(paramA, paramB, paramС) + d2.Invoke(paramA, paramB, paramС);
 
-            figures.Add(Figure.Circle, circle);
-            figures.Add(Figure.Square, square);
-            figures.Add(Figure.Rectangle, rectangle);
-            figures.Add(Figure.Triangle, triangle);
+            figures.Add(Figure.Circle, add(circleArea, circlePerimeter));
+            figures.Add(Figure.Square, add(squareArea, squarePerimeter));
+            figures.Add(Figure.Rectangle, add(rectangleArea, rectanglePerimeter));
+            figures.Add(Figure.Triangle, add(triangleArea, trianglePerimeter));
+            
+            // Сonsole output
+            
+            Console.WriteLine($"{new string('-', 20)} Variant #1 {new string('-', 20)}\n");
+            
+            foreach (var figure in figures)
+            {
+                Console.WriteLine(figure.Value(2, 3, 4));
+            }
 
-            figures[Figure.Circle].Invoke(5);
-            figures[Figure.Square].Invoke(5);
-            figures[Figure.Rectangle].Invoke(5, 8);
-            figures[Figure.Triangle].Invoke(5, 8, 4);
+            Console.WriteLine($"{new string('-', 20)} Variant #2 {new string('-', 20)}\n");
+
+            Console.WriteLine(figures[Figure.Circle].Invoke(5));
+            Console.WriteLine(figures[Figure.Square].Invoke(5));
+            Console.WriteLine(figures[Figure.Rectangle].Invoke(5, 8));
+            Console.WriteLine(figures[Figure.Triangle].Invoke(5, 8, 4));
+            
+            Console.ReadKey();
         }
     }
 }
