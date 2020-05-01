23dea409ca2fe3f19c90d1ca5e8d8f905ba94aa8
Roman <roma.ne4aev2013@gmail.com>
2020-05-01 12:41:46 +0300

    HW17

---
+++ HomeWork17/HomeWork17/Figures/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace HomeWork17.Figures
+{
+    public class Circle : BaseFigure
+    {
+        private double _radius = 1;
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value > 0)
+                {
+                    _radius = value;
+                }
+            }
+        }
+        protected override Figure Type => Figure.Circle;
+
+        public override double GetPerimeter()
+        {
+            return Math.Pow(Radius, 2);
+        }
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ $"and circumference {GetCircumference()}\n";
+        }
+    }
+}
---
+++ HomeWork17/HomeWork17/Figures/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace HomeWork17.Figures
+{
+    public class Rectangle : Square
+    {
+        private double _width = 1;
+        public Rectangle(double height, double width) : base(height)
+        {
+            Width = width;
+        }
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+            }
+        }
+        protected override Figure Type => Figure.Rectangle;
+        public override double GetArea()
+        {
+            return SideSize * Width;
+        }
+        public override double GetPerimeter()
+        {
+            return (SideSize + Width) * 2;
+        }
+    }
+}
